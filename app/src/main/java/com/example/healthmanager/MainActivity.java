504c95113e726baf6bed22a3d32a737200ff0593
nandus94 <faguiar94.fa@gmail.com>
2026-03-15 12:46:18 +0100

    INTERFAZ: perfil usuario (responsive) main calendario temporizador actividades menu CODIGO: perfil de usuario. Al comenzar la app sale esta actividad y una vez rellenados los datos se puede avanzar a la main mediante el boton. Además, en el caso de no haber rellenado algún campo sale un toast para avisar y no deja avanzar (formulario). main activity. se muestra el menú

--- app/src/main/java/com/example/healthmanager/MainActivity.java
+++ app/src/main/java/com/example/healthmanager/MainActivity.java
@@ -1,9 +1,8 @@
 package com.example.healthmanager;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,15 +22,11 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        //Esperar 5 segundos y saltar a MainActivity.
-        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(MainActivity.this, TemporizadorActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        }, 5000);
+    }
+    @Override //Método para crear el menú. Falta hacer el evento onClick en el item para que vaya a la actividad correspondiente.
+    public boolean onCreateOptionsMenu (Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
