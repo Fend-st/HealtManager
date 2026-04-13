9e479d83d09efe2cc1f99b2cf52ac68b2b9b33fa
diazf <diazfv5@gmail.com>
2026-04-13 20:24:23 +0200

    rama test commit

---
+++ app/src/test/java/com/example/healthmanager/GestorBDTest.java
@@ -0,0 +1,32 @@
+package com.example.healthmanager;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GestorBDTest {
+
+    @Test
+    public void consultarUsuario() {
+    }
+
+    @Test
+    public void consultarEvento() {
+    }
+
+    @Test
+    public void consultarDia() {
+    }
+
+    @Test
+    public void insertarUsuario() {
+    }
+
+    @Test
+    public void insertarEvento() {
+    }
+
+    @Test
+    public void insertarActividad() {
+    }
+}
