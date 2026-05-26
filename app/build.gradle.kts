8257ea5bafdeaa283deb72a225626f8e59784184
jorge <jorgegmgdeveloper@gmail.com>
2026-05-26 19:44:47 +0200

    Calendario ready

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,14 +39,21 @@ android {
 }
 
 dependencies {
+
     implementation("androidx.viewpager2:viewpager2:1.0.0")
+    implementation("androidx.viewpager:viewpager:1.1.0")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("com.google.android.material:material:1.11.0")
+
+    implementation("com.github.prolificinteractive:material-calendarview:2.0.1")
+
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
